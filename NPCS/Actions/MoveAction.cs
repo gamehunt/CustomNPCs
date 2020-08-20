dfa9289dc87f120549a0b70018a3bdb6b6d88452
gamehunt <mt.demakov@gmail.com>
2020-08-20 11:29:16 +0300

    Formatting fix

--- NPCS/Actions/MoveAction.cs
+++ NPCS/Actions/MoveAction.cs
@@ -1,14 +1,11 @@
 ﻿using Exiled.API.Features;
 using MEC;
 using NPCS.Talking;
-using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 
 namespace NPCS.Actions
 {
-    class MoveAction : NodeAction
+    internal class MoveAction : NodeAction
     {
         public override string Name => "MoveAction";
 
@@ -19,24 +16,29 @@ namespace NPCS.Actions
                 case "forward":
                     npc.Move(Npc.MovementDirection.FORWARD);
                     break;
+
                 case "backward":
                     npc.Move(Npc.MovementDirection.BACKWARD);
                     break;
+
                 case "right":
                     npc.Move(Npc.MovementDirection.RIGHT);
                     break;
+
                 case "left":
                     npc.Move(Npc.MovementDirection.LEFT);
                     break;
+
                 case "jump":
                     npc.ReferenceHub.animationController.Networkspeed = new UnityEngine.Vector2(1, 1);
                     npc.NPCComponent.attached_coroutines.Add(Timing.CallDelayed(float.Parse(args["duration"].Replace('.', ',')), () => npc.ReferenceHub.animationController.NetworkcurAnim = 0));
                     break;
+
                 default:
                     npc.Move(Npc.MovementDirection.NONE);
                     break;
             }
-            npc.NPCComponent.attached_coroutines.Add(Timing.CallDelayed(float.Parse(args["duration"].Replace('.',',')), () => npc.Move(Npc.MovementDirection.NONE)));
+            npc.NPCComponent.attached_coroutines.Add(Timing.CallDelayed(float.Parse(args["duration"].Replace('.', ',')), () => npc.Move(Npc.MovementDirection.NONE)));
         }
     }
 }
