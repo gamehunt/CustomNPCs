5413359a42bc8422b51b597a4772ee879eee351b
gamehunt <mt.demakov@gmail.com>
2020-08-21 12:27:37 +0300

    fixed version

--- NPCS/Plugin.cs
+++ NPCS/Plugin.cs
@@ -19,7 +19,7 @@ namespace NPCS
         public override string Author { get; } = "gamehunt";
         public override string Name { get; } = "CustomNPCs";
         public override string Prefix { get; } = "CNPCS";
-        public override Version Version { get; } = new Version(1, 1, 2);
+        public override Version Version { get; } = new Version(1, 2, 2);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 0);
 
         public override PluginPriority Priority => PluginPriority.Last;
