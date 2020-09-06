bd29381456d3addcd9473525bc662700633efc42
gamehunt <mt.demakov@gmail.com>
2020-09-06 16:28:44 +0300

    One more NRE fix

---
+++ NPCS/Harmony/VersionCheckFix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NPCS.Harmony
+{
+    [HarmonyPatch(typeof(VersionCheck), nameof(VersionCheck.Start))]
+    class VersionCheckFix
+    {
+        private static bool Prefix(VersionCheck __instance)
+        {
+            if(__instance.connectionToClient == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
