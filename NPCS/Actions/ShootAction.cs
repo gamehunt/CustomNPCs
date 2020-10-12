abe21b6bcc519b74e8fc82455543da681ea3ea71
gamehunt <mt.demakov@gmail.com>
2020-10-12 14:40:55 +0300

    Fixed ShootAction again

--- NPCS/Actions/ShootAction.cs
+++ NPCS/Actions/ShootAction.cs
@@ -27,52 +27,44 @@ namespace NPCS.Actions
                         Vector3 heading = (npc.CurrentAIPlayerTarget.Position - npc.NPCPlayer.Position);
                         Quaternion lookRot = Quaternion.LookRotation(heading.normalized);
                         npc.NPCPlayer.Rotations = new Vector2(lookRot.eulerAngles.x, lookRot.eulerAngles.y);
-                        npc.NPCPlayer.ReferenceHub.weaponManager.CallCmdShoot(npc.CurrentAIPlayerTarget.GameObject, "HEAD", npc.NPCPlayer.CameraTransform.forward, npc.NPCPlayer.Position, npc.CurrentAIPlayerTarget.Position);
-                        if (!npc.CurrentAIPlayerTarget.IsAlive)
-                        {
-                            npc.FireEvent(new Events.NPCTargetKilledEvent(npc, npc.CurrentAIPlayerTarget));
-                        }
+                        npc.NPCPlayer.ReferenceHub.weaponManager.CallCmdShoot(p.GameObject, hitbox, npc.NPCPlayer.CameraTransform.forward, npc.NPCPlayer.Position, p.Position);
                     }
                     yield return Timing.WaitForSeconds(Plugin.Instance.Config.NpcFireCooldownMultiplier * npc.NPCPlayer.ReferenceHub.weaponManager._fireCooldown);
                 }
                 else
                 {
                     float cd = 0f;
-                    npc.Follow(npc.CurrentAIPlayerTarget);
-                    if (Vector3.Distance(npc.CurrentAIPlayerTarget.Position, npc.NPCPlayer.Position) <= 1.5f)
+                    npc.Follow(p);
+                    if (Vector3.Distance(p.Position, npc.NPCPlayer.Position) <= 1.5f)
                     {
                         if (npc.NPCPlayer.Role.Is939())
                         {
-                            npc.NPCPlayer.GameObject.GetComponent<Scp939PlayerScript>().CallCmdShoot(npc.CurrentAIPlayerTarget.GameObject);
+                            npc.NPCPlayer.GameObject.GetComponent<Scp939PlayerScript>().CallCmdShoot(p.GameObject);
                         }
                         else
                         {
                             switch (npc.NPCPlayer.Role)
                             {
                                 case RoleType.Scp106:
-                                    npc.NPCPlayer.GameObject.GetComponent<Scp106PlayerScript>().CallCmdMovePlayer(npc.CurrentAIPlayerTarget.GameObject, ServerTime.time);
+                                    npc.NPCPlayer.GameObject.GetComponent<Scp106PlayerScript>().CallCmdMovePlayer(p.GameObject, ServerTime.time);
                                     cd = 2f;
                                     break;
 
                                 case RoleType.Scp173:
-                                    npc.NPCPlayer.GameObject.GetComponent<Scp173PlayerScript>().CallCmdHurtPlayer(npc.CurrentAIPlayerTarget.GameObject);
+                                    npc.NPCPlayer.GameObject.GetComponent<Scp173PlayerScript>().CallCmdHurtPlayer(p.GameObject);
                                     break;
 
                                 case RoleType.Scp049:
-                                    npc.CurrentAIPlayerTarget.Hurt(99999f, DamageTypes.Scp049, npc.NPCPlayer.Nickname);
+                                    p.Hurt(99999f, DamageTypes.Scp049, npc.NPCPlayer.Nickname);
                                     cd = PlayableScps.Scp049.KillCooldown;
                                     break;
 
                                 case RoleType.Scp0492:
                                     npc.NPCPlayer.GameObject.GetComponent<Scp049_2PlayerScript>().CallCmdShootAnim();
-                                    npc.NPCPlayer.GameObject.GetComponent<Scp049_2PlayerScript>().CallCmdHurtPlayer(npc.CurrentAIPlayerTarget.GameObject);
+                                    npc.NPCPlayer.GameObject.GetComponent<Scp049_2PlayerScript>().CallCmdHurtPlayer(p.GameObject);
                                     cd = 1f;
                                     break;
                             }
-                        }
-                        if (!npc.CurrentAIPlayerTarget.IsAlive)
-                        {
-                            npc.FireEvent(new Events.NPCTargetKilledEvent(npc, npc.CurrentAIPlayerTarget));
                         }
                         npc.Stop();
                     }
